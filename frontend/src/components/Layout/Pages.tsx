0e4e3cf16c37a46f0bcad66835e915b32041e93a
Lukas Corona <luk.corona@gmail.com>
2023-09-23 20:19:45 +0200

    confetti 🎉

--- frontend/src/components/Layout/Pages.tsx
+++ frontend/src/components/Layout/Pages.tsx
@@ -7,51 +7,49 @@ import { sidebarWidth } from '@/config'
 import { Route as RouteItem, adminRoutes, authenticatedRoutes, publicRoutes } from '@/config/routes'
 import { useNotifications } from '@/features/Feedback'
 import useWindow from '@/hooks/useWindow'
-import { sidebarAtom } from '@/utils/atoms'
+import { confettiAtom, sidebarAtom } from '@/utils/atoms'
 import ErrorBoundary from './ErrorBoundary'
 import { RequireAdmin, RequireAuth } from './RouteGards'
+import Confetti from 'react-confetti'
 
 export default function Pages() {
-  const navigate = useNavigate()
-  const addNotification = useNotifications((state) => state.addNotification)
-  const [sidebarOpen] = useAtom(sidebarAtom)
-  const { width } = useWindow()
-  const theme = useTheme()
+    const navigate = useNavigate()
+    const addNotification = useNotifications((state) => state.addNotification)
+    const [sidebarOpen] = useAtom(sidebarAtom)
+    const [showConfetti] = useAtom(confettiAtom)
+    const { width } = useWindow()
+    const theme = useTheme()
 
-  const marginTop = useMemo(() => Number(theme.mixins.toolbar.minHeight || 0), [theme])
-  const marginLeft = useMemo(
-    () => (sidebarOpen ? sidebarWidth : width < 600 ? 0 : 72),
-    [sidebarOpen, width],
-  )
+    const marginTop = useMemo(() => Number(theme.mixins.toolbar.minHeight || 0), [theme])
+    const marginLeft = useMemo(() => (sidebarOpen ? sidebarWidth : width < 600 ? 0 : 72), [sidebarOpen, width])
 
-  // setup axios interceptors
-  useEffect(() => {
-    setupInterceptors(navigate, addNotification)
-    return () => ejectInterceptors()
-  }, [addNotification, navigate])
+    // setup axios interceptors
+    useEffect(() => {
+        setupInterceptors(navigate, addNotification)
+        return () => ejectInterceptors()
+    }, [addNotification, navigate])
 
-  const renderRoutes = (routes: RouteItem[]) =>
-    routes.map(({ path, component: Component }) => (
-      <Route key={path} path={path} element={<Component />} />
-    ))
+    const renderRoutes = (routes: RouteItem[]) =>
+        routes.map(({ path, component: Component }) => <Route key={path} path={path} element={<Component />} />)
 
-  return (
-    <Box
-      component="main"
-      sx={{
-        height: `calc(100vh - ${marginTop}px)`,
-        marginTop: `${marginTop}px`,
-        marginLeft: `${marginLeft}px`,
-        padding: theme.spacing(2),
-      }}
-    >
-      <ErrorBoundary>
-        <Routes>
-          {renderRoutes(publicRoutes)}
-          <Route element={<RequireAuth />}>{renderRoutes(authenticatedRoutes)}</Route>
-          <Route element={<RequireAdmin />}>{renderRoutes(adminRoutes)}</Route>
-        </Routes>
-      </ErrorBoundary>
-    </Box>
-  )
+    return (
+        <Box
+            component="main"
+            sx={{
+                height: `calc(100vh - ${marginTop}px)`,
+                marginTop: `${marginTop}px`,
+                marginLeft: `${marginLeft}px`,
+                padding: theme.spacing(2),
+            }}
+        >
+            <ErrorBoundary>
+                {showConfetti && <Confetti />}
+                <Routes>
+                    {renderRoutes(publicRoutes)}
+                    <Route element={<RequireAuth />}>{renderRoutes(authenticatedRoutes)}</Route>
+                    <Route element={<RequireAdmin />}>{renderRoutes(adminRoutes)}</Route>
+                </Routes>
+            </ErrorBoundary>
+        </Box>
+    )
 }
--- frontend/src/utils/atoms.tsx
+++ frontend/src/utils/atoms.tsx
@@ -1,5 +1,8 @@
 import { atom } from 'jotai'
+import { atomWithStorage } from 'jotai/utils'
 
 export const sidebarAtom = atom(false)
+
+export const confettiAtom = atomWithStorage<boolean | undefined>('confetti', !!localStorage.getItem('confetti'))
 
 export const currentTournamentAtom = atom(1)
