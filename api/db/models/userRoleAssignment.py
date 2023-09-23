4d4090ad01ad41bf469859ed97aac528ce105dba
TJReichard <reichard.thiemo@gmail.com>
2023-09-23 21:11:22 +0200

    feat: sql init for test data

--- api/db/models/userRoleAssignment.py
+++ api/db/models/userRoleAssignment.py
@@ -5,7 +5,7 @@ from db.base import Base
 class UserRoleAssignment(Base):
     userId = Column(Integer, ForeignKey('user.id'), nullable=False, primary_key=True)
     roleId = Column(Integer, ForeignKey('role.id'), nullable=False, primary_key=True)
-    conferenceId = Column(Integer, ForeignKey('conference.id'), nullable=False, primary_key=True) #potential primary key issue because of duplicates
+    conferenceId = Column(Integer, ForeignKey('conference.id'), nullable=False, primary_key=True)
                                               
     user = relationship("user", backref="conferenceApplication")
     conference = relationship("conference", backref="conferenceApplication")
