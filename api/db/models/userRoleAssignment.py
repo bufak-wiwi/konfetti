2b64f1310c0f94cc5caa06245d3a03bc6ef2dee8
hrdominik <dominik@hoehr.net>
2023-09-24 12:24:26 +0200

    fix: userpermission sqlsearch

--- api/db/models/userRoleAssignment.py
+++ api/db/models/userRoleAssignment.py
@@ -3,9 +3,10 @@ from sqlalchemy.orm import relationship
 from db.base import Base
 
 class UserRoleAssignment(Base):
+    id = Column(Integer, primary_key=True, autoincrement=True)
     userId = Column(Integer, ForeignKey('user.id'), nullable=False, primary_key=True)
     roleId = Column(Integer, ForeignKey('role.id'), nullable=False, primary_key=True)
-    conferenceId = Column(Integer, ForeignKey('conference.id'), nullable=False, primary_key=True)
+    conferenceId = Column(Integer, ForeignKey('conference.id'))
                                               
     user = relationship("user", backref="conferenceApplication")
     conference = relationship("conference", backref="conferenceApplication")
