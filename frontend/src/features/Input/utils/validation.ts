487b74fbc85653c643c6c0d5e01d97caf586b7a1
Lukas Corona <luk.corona@gmail.com>
2024-01-13 10:30:13 +0100

    fix: use correct dev proxy url

--- frontend/src/features/Input/utils/validation.ts
+++ frontend/src/features/Input/utils/validation.ts
@@ -1,12 +1,12 @@
 export const isValidEmail = (mail: string) => {
-  const re =
-    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-  return re.test(mail?.toLowerCase())
+    const re =
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    return re.test(mail?.toLowerCase())
 }
 
 /**
  * Check whether the password is between 8 and 32 characters long
  */
 export const isValidPassword = (password: string) => {
-  return password.length >= 8 && password.length <= 32
+    return password.length <= 32 // TODO: && password.length >= 8
 }
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,13 +2,21 @@ import { defineConfig } from 'vite'
 import * as path from 'path'
 import react from '@vitejs/plugin-react'
 
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './src'),
+        },
     },
-  },
+    server: {
+        proxy: {
+            '/api': {
+                target: 'http://127.0.0.1:8000',
+                changeOrigin: true,
+                secure: false,
+            },
+        },
+    },
 })
