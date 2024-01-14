d9cf025707234f38058dd2fe3522b6d2443829ac
Phillip Püttmann <cebit-fan@web.de>
2024-01-14 01:33:09 +0100

    add endpoints to api router

--- api/endpoints/api.py
+++ api/endpoints/api.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter
 
-from endpoints import auth
-from endpoints import user
+from endpoints import auth, user, votingAnswer, votingQuestion
 
 api_router = APIRouter()
 
 # map all endpoints to routeprefixes
 api_router.include_router(auth.router, prefix="", tags=["auth"])
 api_router.include_router(user.router, prefix="/users", tags=["users"])
+api_router.include_router(votingQuestion.router, prefix="/votingQuestions", tags=["voting"])
+api_router.include_router(votingAnswer.router, prefix="/votingAnswers", tags=["voting"])
